8e5153c8b826d8711b47b1af3a244f33b96ed022
RonFrie <agnet.ronny@icloud.com>
2024-04-13 17:48:28 +0200

    Testing

--- src/main/java/de/hka_iwi_1/avg_s2_producer/repository/DB.java
+++ src/main/java/de/hka_iwi_1/avg_s2_producer/repository/DB.java
@@ -1,16 +1,16 @@
-
-
 package de.hka_iwi_1.avg_s2_producer.repository;
 
 import de.hka_iwi_1.avg_s2_producer.entity.Share;
 import de.hka_iwi_1.avg_s2_producer.entity.StockMarket;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,7 +27,7 @@ static {
                             Share.builder()
                                     .availableShares(100)
                                     .price(new BigDecimal("124.09"))
-                                    .prices24hrs()
+                                    .prices24hrs(Arrays.asList(new BigDecimal("124.03"), new BigDecimal("123.99")))
                                     .wkn("123456")
                                     .build()
                     ).collect(Collectors.toList()))
@@ -39,7 +39,7 @@ static {
                             Share.builder()
                                     .availableShares(200)
                                     .price(new BigDecimal("384.83"))
-                                    .prices24hrs()
+                                    .prices24hrs(Arrays.asList(new BigDecimal("384.85"), new BigDecimal("384.95")))
                                     .wkn("234567")
                                     .build()
                     ).collect(Collectors.toList()))
