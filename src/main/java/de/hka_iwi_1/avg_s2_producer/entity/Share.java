d2c214834e15653c15b3bfe2dc15424d058d6cf0
RonFrie <agnet.ronny@icloud.com>
2024-04-13 16:27:56 +0200

    Added the entities Share and StockMarket

--- src/main/java/de/hka_iwi_1/avg_s2_producer/entity/Share.java
+++ src/main/java/de/hka_iwi_1/avg_s2_producer/entity/Share.java
@@ -1,5 +1,6 @@
 package de.hka_iwi_1.avg_s2_producer.entity;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 import java.math.BigDecimal;
 import java.util.Collection;
 
+@Builder
 @Getter
 @Setter
 @ToString
--- src/main/java/de/hka_iwi_1/avg_s2_producer/entity/StockMarket.java
+++ src/main/java/de/hka_iwi_1/avg_s2_producer/entity/StockMarket.java
@@ -1,5 +1,6 @@
 package de.hka_iwi_1.avg_s2_producer.entity;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.ToString;
 import java.util.List;
 import java.util.UUID;
 
+@Builder
 @Getter
 @Setter
 @ToString
